e22091032e0f06ec7f2cce5e5f53ff0d4e1dd9b0
AlbertBeto <apbaleyto@gmail.com>
2023-11-22 19:18:43 +0100

    Temporizadores con ejercicio fallo

--- ud5ObjetoNavegador.js
+++ ud5ObjetoNavegador.js
@@ -40,7 +40,7 @@ console.log(Date.now());
 //Esto nos devuelve el date de cuando se creo fecha
 console.log(Date.parse(fecha))
 */
-
+/*
 //EXPRESIONES REGULARES
 let cad = "En un lugar de la Mancha, de cuyo nombre no quiero acordarme...";
 
@@ -79,3 +79,50 @@ let email_simple = "albert@ggg.coms";
 let expresion_email_simple = /^[a-z]+[@][a-z]+[.]([com|org|es])+$/i
 document.write("Email"+"<br>");
 document.write(expresion_email_simple.test(email_simple)+"<br>");
+*/
+
+//TIMERS
+//Aqui primero imprimimos en la consola la hora y luego la voilvemos a imprimi 5 segundos despues. 
+//Con console log no hay problema pero si lo hacemos document.write sacaria el primero pero como 
+//cierra el flujo a los 5 segundos hace un decoument.write fuera de flujo con lo que limpia todo el html
+// pega el horario pero se ha cargado el flujo HTML anterior. 
+//POR ESTO NO HAY QUE USAR EL DOCUMENT.WRITE
+/*
+document.write(new Date().toString()); // Imprime inmediatamente la fecha actual
+//Guardamos el temporizador en una variable
+let temporizadorUno = setTimeout(() => document.write(new Date().toString()), 5000); // Se ejecutará en 5 segundos(5000 ms);
+//Borro el temporizador con la varible anterior. 
+clearTimeout(temporizadorUno);
+*/
+
+
+//El ejercicio esta mal. Hay que crear la idInterval en funcion. 
+let intervalo = prompt("En que intervalos de segundos quieres mostrar en pantalla?");
+let maximo_segundos = prompt("En que segundo quieres que pare?");
+let num = 0;
+let aviso = "";
+let intervaloInt = parseInt(intervalo);
+let maximo_segundosInt = parseInt(maximo_segundos);
+//setInterval(() => console.log(num++), 1000); // Imprime un número y lo incrementa cada segundo
+function idInterval(){ setInterval(() => {
+    console.log(num = num + intervaloInt);
+    if (num >= maximo_segundosInt) { // Cuando imprimimos 10, paramos el timer para que no se repita más
+        alert("Ha completado la cuenta de " + maximo_segundosInt);
+        num = 0;
+        maximo_segundosInt=1;
+        aviso = prompt("Quiere volver hacer cuenta atras s/n?");
+        if (aviso == "s"){
+           
+            num = 0;
+            maximo_segundosInt=1;
+            idInterval();
+        
+        } else { clearInterval(idInterval); }}
+}, intervaloInt * 1000);}
+
+
+idInterval();
+
+function preguntar(){
+    let t1 =parseInt
+}
